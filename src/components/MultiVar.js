682c9453e4125ec63e1f8ddc9bc400bad8e261a8
Smruthi Gowtham <smruthig01@gmail.com>
2021-07-29 23:19:00 +0530

    message changed

--- src/components/MultiVar.js
+++ src/components/MultiVar.js
@@ -2,8 +2,7 @@ const MultiVar = () => {
     return (
         <div>
             <h6 style={{color:'white'}}>If you want to superimpose multiple sets of data, 
-            you can do so individually for the following chart types: <br/>
-            Radar, Grouped Bar chart, Line chart</h6>
+            you can do so individually for Grouped Bar chart</h6>
         </div>
     )
 }
